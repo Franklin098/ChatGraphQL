0377f050ff445ff5c152a7036e5ebf124d739e79
Franklin Velasquez <iefranklin@hotmail.es>
2023-01-22 20:51:27 -0600

    update cache by appending new message

--- client/src/graphql/hooks.js
+++ client/src/graphql/hooks.js
@@ -1,15 +1,31 @@
-import { useMutation, useQuery } from '@apollo/client';
-import { getAccessToken } from '../auth';
-import { ADD_MESSAGE_MUTATION, MESSAGES_QUERY } from './queries';
+import { useMutation, useQuery } from "@apollo/client";
+import { getAccessToken } from "../auth";
+import { ADD_MESSAGE_MUTATION, MESSAGES_QUERY } from "./queries";
 
 export function useAddMessage() {
   const [mutate] = useMutation(ADD_MESSAGE_MUTATION);
   return {
     addMessage: async (text) => {
-      const { data: { message } } = await mutate({
+      const {
+        data: { message },
+      } = await mutate({
         variables: { input: { text } },
         context: {
-          headers: { 'Authorization': 'Bearer ' + getAccessToken() },
+          headers: { Authorization: "Bearer " + getAccessToken() },
+        },
+        update: (cache, result) => {
+          // update cache with the created message
+          const {
+            data: { message },
+          } = result;
+          // pass the query to update
+          cache.updateQuery({ query: MESSAGES_QUERY }, (oldData) => {
+            // update adding the new message.
+            const newData = {
+              messages: [...oldData.messages, message],
+            };
+            return newData;
+          });
         },
       });
       return message;
@@ -20,7 +36,7 @@ export function useAddMessage() {
 export function useMessages() {
   const { data } = useQuery(MESSAGES_QUERY, {
     context: {
-      headers: { 'Authorization': 'Bearer ' + getAccessToken() },
+      headers: { Authorization: "Bearer " + getAccessToken() },
     },
   });
   return {
